7ce7702fe9348de884e347bb3e5bf7c7a0c5e3b1
Jorge Valencia <info@jorgevalencia.co>
2016-05-08 15:01:38 +0200

    ejercicio

---
+++ assets/js/menu.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+   function updateContainer() {
+      // menu mobile  - Desplegear submenu
+      if ($(window).width() < 990) {
+         $('#menu-container li').click(function(e) {
+            $(this).find('.sub-menu').fadeToggle();
+            $(this).siblings().find('.sub-menu:visible').hide();
+         });
+      // menu desktop - Desplegear submenu
+      } else {
+         $('#menu-container li').hover(function(e) {
+            $(this).find('.sub-menu').fadeToggle();
+         });
+      }
+   }
+
+   updateContainer();
+   
+   $(window).resize(function() {
+      updateContainer();
+   });
+
+   // Resaltar <li> clickeado
+   $('#menu-container a').click(function(e) {
+       $('#menu-container li').removeClass('current-menu-item');
+       $(this).parent().addClass('current-menu-item');
+   });
+
+   // Desplegear submenu
+   // $('#menu-container li').hover(function(e) {
+   //    $(this).find('.sub-menu').fadeToggle();
+   // });
+
+   // Abrir menu mobile
+   $('#mobile-button').click(function(e) {
+      $('#menu-container').show();
+      $(this).hide();
+      $('#mobile-close').show();
+   })
+
+   // Cerrar menu mobile
+   $('#mobile-close').click(function(e) {
+      $('#menu-container').hide();
+      $(this).hide();
+      $('#mobile-button').show();
+   })
+
+   // // menu mobile  - Desplegear submenu
+   // if ($(window).width() < 990) {
+   //    $('#menu-container li').click(function(e) {
+   //       $(this).find('.sub-menu').fadeToggle();
+   //       $(this).siblings().find('.sub-menu:visible').hide();
+   //    });
+   // // menu desktop - Desplegear submenu
+   // } else {
+   //    $('#menu-container li').hover(function(e) {
+   //       $(this).find('.sub-menu').fadeToggle();
+   //    });
+   // }
+
+
+
+
+});
